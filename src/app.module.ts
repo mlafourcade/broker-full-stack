34ee9c6ba118fba5cb622facd74abca9685d1685
marciolafourcade <marciolafourcade@gmail.com>
2024-02-05 15:06:58 -0300

    Testes com docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      etities: [Product]
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column()
+    name: string;
+    @Column({ type: 'text'})
+    description: string;
+    @Column()
+    image_url: string;
+    @Column()
+    price: number;
+}
+
+
